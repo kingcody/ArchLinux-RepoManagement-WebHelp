15ef340d1f18a0d473d62dd50b1ce9e08be4fc0e
Cody Mize <me@codymize.com>
2013-11-26 03:53:37 -0500

    Update index.php
    commenting out Google analytics and changing page title

--- repos/index.php
+++ repos/index.php
@@ -61,7 +61,7 @@ Server = " . $repo['server'] . "</pre>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Digitally Seamless ArchLinux Repositories</title>
+        <title>My Linux Repositories</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -108,6 +108,7 @@ Server = " . $repo['server'] . "</pre>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <!--
         <script>
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
             function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
@@ -116,5 +117,6 @@ Server = " . $repo['server'] . "</pre>
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X');ga('send','pageview');
         </script>
+        -->
     </body>
 </html>
